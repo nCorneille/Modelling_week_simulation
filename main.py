a5e55a495316830c9e46573ca6defd958df27aa2
nCorneille <n.corneille@student.tue.nl>
2021-11-10 17:15:59 +0100

    Implemented multiple separate machines

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ MCH_POWER = 3000
 START_STATE = m
 
 LAMBDA = 5e-3
-MU = 1
+MU = 0.01
 
 model_2_mch = MarkovChainModel("matrix_2",
                               {"lambda": "LAMBDA", "mu": "MU", "exp": "exp"},
@@ -50,10 +50,11 @@ model_2_mch = MarkovChainModel("matrix_2",
 mat = make_matrix(m, LAMBDA, MU)
 model_2_mch.change_matrix(mat)
 
-model_2_mch_sim = TransitionHandler(model_2_mch)
+model_2_mch_sim = TransitionHandler(model_2_mch, MCH_POWER, m)
+model_2_mch_sim2 = TransitionHandler(model_2_mch, MCH_POWER, m)
 
-buffer_model_2_mch = BufferSimulationHandler(model_2_mch, MAX_BUFFER)
-buffer = buffer_model_2_mch.buffer_simulation(START_STATE, START_BUFFER, MAX_ITER, demand_data, MCH_POWER)
+buffer_model_2_mch = BufferSimulationHandler([model_2_mch_sim, model_2_mch_sim2], MAX_BUFFER)
+buffer = buffer_model_2_mch.buffer_simulation(START_STATE, START_BUFFER, MAX_ITER, demand_data)
 print(buffer[0])
 print(buffer[1])
 
@@ -62,4 +63,4 @@ L = SequenceAnalyser.get_length_true_sequences(np.array(buffer[1]) == 0, 1)
 print(L[0])
 print(FineHandler(lambda x: x).to_days_between(L[0], L[1]))
 
-print(FineHandler(fine).calculate_fine(L[0],L[1]))
+print(FineHandler(fine).calculate_fine(L[0], L[1]))
--- simulation_handler.py
+++ simulation_handler.py
@@ -2,14 +2,19 @@ from matrix_handler import *
 
 
 class TransitionHandler:
-    def __init__(self, markov_model: MarkovChainModel):
+    def __init__(self, markov_model: MarkovChainModel, supply_per_machine, start_state):
         self.markov_model = markov_model
         self.transition_matrix = markov_model.matrix
+        self.cur_state = start_state
+        self.supply_per_machine = supply_per_machine
 
     def choose_next_state(self, current_state: int, m: int):
         row = self.transition_matrix
-        next_state = np.random.choice([i for i in range(m)], 1, p=[row[current_state, i] for i in range(m)])
+        next_state = np.random.choice([i for i in range(m)], 1, p=[row[current_state, i] for i in range(m)])[0]
         return next_state
+
+    def update_state(self, m):
+        self.cur_state = self.choose_next_state(self.cur_state, m)
 
     def transition_simulation(self, start_state, iterations):
         i = 0
@@ -26,29 +31,31 @@ class TransitionHandler:
         return states
 
 
-class BufferSimulationHandler(TransitionHandler):
-    def __init__(self, markov_model: MarkovChainModel, maximum_size=1 << 31, minimum_size=0):
-        super().__init__(markov_model)
+class BufferSimulationHandler():
+    def __init__(self, markov_models, maximum_size=1 << 31, minimum_size=0):
+        self.markov_models = markov_models
         self.minimum_size = minimum_size
         self.maximum_size = maximum_size
 
-    def buffer_simulation(self, start_state, init_buffer, max_iterations, demand, supply_per_machine):
-        cur_state = start_state
+    def buffer_simulation(self, start_state, init_buffer, max_iterations, demand):
         cur_buffer = init_buffer
 
         buffer = [cur_buffer]
-        states = [start_state]
+        states = []
+        states.append([model.cur_state for model in self.markov_models])
         i = 0
 
         while i < max_iterations:
-            cur_buffer += cur_state * supply_per_machine - demand[i % len(demand)]
-            cur_state = self.choose_next_state(cur_state, self.transition_matrix.shape[0])[0]
+            for model in self.markov_models:
+                cur_buffer += model.cur_state * model.supply_per_machine
+                model.update_state(model.transition_matrix.shape[0])
 
+            cur_buffer -= demand[i % len(demand)]
             cur_buffer = max(self.minimum_size, cur_buffer)
             cur_buffer = min(self.maximum_size, cur_buffer)
 
             buffer.append(cur_buffer)
-            states.append(cur_state)
+            states.append([model.cur_state for model in self.markov_models])
 
             i += 1
 
