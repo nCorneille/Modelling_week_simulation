dd39c12dab6d6a5b10bcf6b93b79e5ca874953b3
nCorneille <n.corneille@student.tue.nl>
2021-11-11 09:53:48 +0100

    Added machine policies for whether they should be on or off

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from matrix_handler import *
 from sequence_analysis import *
 from fine_handler import *
 
+import matplotlib.pyplot as plt
+
 
 def csv_reader(filename):
     filename += ".csv"
@@ -19,48 +21,104 @@ def csv_reader(filename):
 
 
 def fine(T):
-    return 0 if T < 8 else 35 + np.floor((T-8)/4) * 20
+    return 0 if T < 8 else 35 + np.floor((T - 8) / 4) * 20
 
-#Failure rates based on Badami et al.
-#Boilers:
-#lambda = 5 * 10^-3
-#mu = 1
+
+# Failure rates based on Badami et al.
+# Boilers:
+# lambda = 5 * 10^-3
+# mu = 1
 #
-#Pumps:
-#lambda = 1 * 10^-5
-#mu = 0.083
+# Pumps:
+# lambda = 1 * 10^-5
+# mu = 0.083
 
-m = 10
+
 demand_data = csv_reader("demand_data")
-
 
 MAX_ITER = 24 * 365
 START_BUFFER = 0
 MAX_BUFFER = 644000
-MCH_POWER = 3000
-START_STATE = m
 
-LAMBDA = 5e-3
-MU = 0.01
+# MACHINE 1: HEAT PUMPS
+MACHINE_1_AMOUNT = 5
+MACHINE_1_FAIL_RATE = 1e-5
+MACHINE_1_REPAIR_RATE = 0.083
+MACHINE_1_START_STATE = MACHINE_1_AMOUNT
+MACHINE_1_POWER = 1800
 
-model_2_mch = MarkovChainModel("matrix_2",
-                              {"lambda": "LAMBDA", "mu": "MU", "exp": "exp"},
-                              {"exp": np.exp, "LAMBDA": LAMBDA, "MU": MU})
+machine_1_mm = MarkovChainModel()
+machine_1_matrix = make_matrix(MACHINE_1_AMOUNT, MACHINE_1_FAIL_RATE, MACHINE_1_REPAIR_RATE)
+machine_1_mm.change_matrix(machine_1_matrix)
+machine_1_transition_handler = TransitionHandler(machine_1_mm, MACHINE_1_POWER, MACHINE_1_AMOUNT)
 
-mat = make_matrix(m, LAMBDA, MU)
-model_2_mch.change_matrix(mat)
 
-model_2_mch_sim = TransitionHandler(model_2_mch, MCH_POWER, m)
-model_2_mch_sim2 = TransitionHandler(model_2_mch, MCH_POWER, m)
+def GAS_BOILER_POLICY(current_buffer, maximum_buffer):
+    #return 1-current_buffer / maximum_buffer
+    return 0 if current_buffer/maximum_buffer > 0.5 else 1
 
-buffer_model_2_mch = BufferSimulationHandler([model_2_mch_sim, model_2_mch_sim2], MAX_BUFFER)
-buffer = buffer_model_2_mch.buffer_simulation(START_STATE, START_BUFFER, MAX_ITER, demand_data)
-print(buffer[0])
-print(buffer[1])
 
+# MACHINE 2: CHP
+MACHINE_2_AMOUNT = 4
+MACHINE_2_FAIL_RATE = 1.65e-3
+MACHINE_2_REPAIR_RATE = 0.106
+MACHINE_2_START_STATE = MACHINE_2_AMOUNT
+MACHINE_2_POWER = 900
+
+machine_2_mm = MarkovChainModel()
+machine_2_matrix = make_matrix(MACHINE_2_AMOUNT, MACHINE_2_FAIL_RATE, MACHINE_2_REPAIR_RATE)
+machine_2_mm.change_matrix(machine_2_matrix)
+machine_2_transition_handler = TransitionHandler(machine_2_mm, MACHINE_2_POWER, MACHINE_2_AMOUNT, GAS_BOILER_POLICY)
+
+# MACHINE 3: GAS BOILERS
+MACHINE_3_AMOUNT = 3
+MACHINE_3_FAIL_RATE = 5e-3
+MACHINE_3_REPAIR_RATE = 1
+MACHINE_3_START_STATE = MACHINE_3_AMOUNT
+MACHINE_3_POWER = 6000
+
+machine_3_mm = MarkovChainModel()
+machine_3_matrix = make_matrix(MACHINE_3_AMOUNT, MACHINE_3_FAIL_RATE, MACHINE_3_REPAIR_RATE)
+machine_3_mm.change_matrix(machine_3_matrix)
+machine_3_transition_handler = TransitionHandler(machine_3_mm, MACHINE_3_POWER, MACHINE_3_AMOUNT, GAS_BOILER_POLICY)
+
+machines = [machine_1_transition_handler, machine_2_transition_handler, machine_3_transition_handler]
+buffer_simulation_handler = BufferSimulationHandler(machines, MAX_BUFFER)
+# buffer = buffer_simulation_handler.buffer_simulation(START_BUFFER, MAX_ITER, demand_data)
+# print(buffer[0])
+# print(buffer[1])
+#
+# L = SequenceAnalyser.get_length_true_sequences(np.array(buffer[1]) == 0, 1)
+#
+# print(L[0])
+# print(FineHandler(lambda x: x).to_days_between(L[0], L[1]))
+#
+# print(FineHandler(fine).calculate_fine(L[0], L[1]))
+
+buffer = buffer_simulation_handler.buffer_simulation(START_BUFFER, MAX_ITER, demand_data)
 L = SequenceAnalyser.get_length_true_sequences(np.array(buffer[1]) == 0, 1)
+fines = [FineHandler(fine).calculate_fine(L[0], L[1])]
 
-print(L[0])
-print(FineHandler(lambda x: x).to_days_between(L[0], L[1]))
+for i in range(10):
+    print(i)
+    MACHINE_2_FAIL_RATE += 0.1
+    MACHINE_3_FAIL_RATE += 0.1
 
-print(FineHandler(fine).calculate_fine(L[0], L[1]))
+    machine_2_matrix = make_matrix(MACHINE_2_AMOUNT, MACHINE_2_FAIL_RATE, MACHINE_2_REPAIR_RATE)
+    machine_2_mm.change_matrix(machine_2_matrix)
+    machine_2_transition_handler = TransitionHandler(machine_2_mm, MACHINE_2_POWER, MACHINE_2_AMOUNT, GAS_BOILER_POLICY)
+
+    machine_3_matrix = make_matrix(MACHINE_3_AMOUNT, MACHINE_3_FAIL_RATE, MACHINE_3_REPAIR_RATE)
+    machine_3_mm.change_matrix(machine_3_matrix)
+    machine_3_transition_handler = TransitionHandler(machine_3_mm, MACHINE_3_POWER, MACHINE_3_AMOUNT, GAS_BOILER_POLICY)
+
+    machines = [machine_1_transition_handler, machine_2_transition_handler, machine_3_transition_handler]
+    buffer_simulation_handler = BufferSimulationHandler(machines, MAX_BUFFER)
+
+    buffer = buffer_simulation_handler.buffer_simulation(START_BUFFER, MAX_ITER, demand_data)
+    L = SequenceAnalyser.get_length_true_sequences(np.array(buffer[1]) == 0, 1)
+    fines.append(FineHandler(fine).calculate_fine(L[0], L[1]))
+
+print(fines)
+
+
--- simulation_handler.py
+++ simulation_handler.py
@@ -2,11 +2,13 @@ from matrix_handler import *
 
 
 class TransitionHandler:
-    def __init__(self, markov_model: MarkovChainModel, supply_per_machine, start_state):
+    def __init__(self, markov_model: MarkovChainModel, supply_per_machine, start_state, policy=lambda x, y: 1):
         self.markov_model = markov_model
         self.transition_matrix = markov_model.matrix
         self.cur_state = start_state
         self.supply_per_machine = supply_per_machine
+        self.original_supply_per_machine = supply_per_machine
+        self.policy = policy
 
     def choose_next_state(self, current_state: int, m: int):
         row = self.transition_matrix
@@ -16,19 +18,25 @@ class TransitionHandler:
     def update_state(self, m):
         self.cur_state = self.choose_next_state(self.cur_state, m)
 
-    def transition_simulation(self, start_state, iterations):
+    def transition_simulation(self, start_state, iterations, m):
         i = 0
         cur_state = start_state
         states = [start_state]
 
         while i < iterations:
-            next_state = self.choose_next_state(cur_state)[0]
+            next_state = self.choose_next_state(cur_state, m)[0]
             cur_state = next_state
 
             states.append(cur_state)
             i += 1
 
         return states
+
+    def scale_machine_supply(self, scale):
+        self.supply_per_machine = scale * self.original_supply_per_machine
+
+    def apply_policy(self, current_buffer, maximum_buffer):
+        self.scale_machine_supply(self.policy(current_buffer, maximum_buffer))
 
 
 class BufferSimulationHandler():
@@ -37,16 +45,16 @@ class BufferSimulationHandler():
         self.minimum_size = minimum_size
         self.maximum_size = maximum_size
 
-    def buffer_simulation(self, start_state, init_buffer, max_iterations, demand):
+    def buffer_simulation(self, init_buffer, max_iterations, demand):
         cur_buffer = init_buffer
 
         buffer = [cur_buffer]
-        states = []
-        states.append([model.cur_state for model in self.markov_models])
+        states = [[model.cur_state for model in self.markov_models]]
         i = 0
 
         while i < max_iterations:
             for model in self.markov_models:
+                model.apply_policy(cur_buffer, self.maximum_size)
                 cur_buffer += model.cur_state * model.supply_per_machine
                 model.update_state(model.transition_matrix.shape[0])
 
